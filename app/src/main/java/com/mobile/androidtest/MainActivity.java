e0d2fe195482baffaf67c0a2c42c2ebacff39025
huangjian <huangjian1@hydsoft.com>
2017-08-25 15:21:07 +0800

    启动页适配虚拟键盘遮挡问题

--- app/src/main/java/com/mobile/androidtest/MainActivity.java
+++ app/src/main/java/com/mobile/androidtest/MainActivity.java
@@ -3,6 +3,15 @@ package com.mobile.androidtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ *启动页底部logo被导航栏遮挡。
+ *  解决方案：这个问题只有在5.0+（21）才会出现。
+ *  在values-v21/styles.xml样式中启动页样式设置
+ *  ：<item name="android:windowDrawsSystemBarBackgrounds">false</item>
+ *  参考：https://juejin.im/entry/57fe42a8da2f60004fb4c311
+ *  https://stackoverflow.com/questions/40814657/can-a-windowbackground-be-positioned-below-the-status-bar-above-the-navigation
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
