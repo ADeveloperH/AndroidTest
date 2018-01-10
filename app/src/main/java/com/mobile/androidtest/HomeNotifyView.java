fce46077380faec3b52c3d2d6709d7aa19bd038b
huangjian <huangjian1@hydsoft.com>
2018-01-10 12:29:17 +0800

    动画效果完成

--- app/src/main/java/com/mobile/androidtest/HomeNotifyView.java
+++ app/src/main/java/com/mobile/androidtest/HomeNotifyView.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.os.Message;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
