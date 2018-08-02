66435d554a3330f747c3234db00becae1fa9c32e
huangjian <huangjian@credittag.io>
2018-08-02 10:21:21 +0800

    Arouter 使用Demo，实现跨模块调用

--- app/src/main/java/com/mobile/androidtest/InModuleActivity.java
+++ app/src/main/java/com/mobile/androidtest/InModuleActivity.java
@@ -1,10 +1,14 @@
 package com.mobile.androidtest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
+import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.example.baselibrary.UserModuleServiceUtil;
 
 // 为每一个参数声明一个字段，并使用 @Autowired 标注
@@ -12,44 +16,38 @@ import com.example.baselibrary.UserModuleServiceUtil;
 @Route(path = "/test/inmoduleactivity")
 public class InModuleActivity extends AppCompatActivity {
 
-//    @Autowired
-//    public long key1;
-//    @Autowired
-//    public String key2;
-//    @Autowired(name = "key3")
-//    Test obj;
-
-//    @Autowired(name = RouterPathUtil.USER_MODULE_SERVICE)
-//    IUserModuleService iUserModuleService;
+    @Autowired
+    public long key1;
+    @Autowired
+    public String key2;
+    @Autowired(name = "key3")
+    Test obj;
 
     private static final String TAG = "hj";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        ARouter.getInstance().inject(this);
-
-
+        ARouter.getInstance().inject(this);
         setContentView(R.layout.activity_in_module);
-//        Intent intent = getIntent();
-//        if (intent != null) {
-//            long key1 = intent.getLongExtra("key1", -1l);
-//            String key2 = intent.getStringExtra("key2");
-//            String obj = intent.getStringExtra("obj");
-//
-//            Log.d(TAG, "onCreate: key1:" + key1);
-//            Log.d(TAG, "onCreate: key2:" + key2);
-//            Log.d(TAG, "onCreate: obj:" + obj);
-//
-//        }
 
-//        Log.d(TAG, "onCreate: key1:" + key1);
-//        Log.d(TAG, "onCreate: key2:" + key2);
-//        if (obj != null) {
-//            Log.d(TAG, "onCreate: obj:" + obj.toString());
-//        } else {
-//            Log.d(TAG, "onCreate: obj:" + obj);
-//        }
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            long key1 = intent.getLongExtra("key1", -1l);
+            String key2 = intent.getStringExtra("key2");
+
+            Log.d(TAG, "onCreate: key1:" + key1);
+            Log.d(TAG, "onCreate: key2:" + key2);
+        }
+
+        Log.d(TAG, "onCreate: key1:" + key1);
+        Log.d(TAG, "onCreate: key2:" + key2);
+        if (obj != null) {
+            Log.d(TAG, "onCreate: obj:" + obj.toString());
+        } else {
+            Log.d(TAG, "onCreate: obj:" + obj);
+        }
 
 
         findViewById(R.id.btn_invoke_method).setOnClickListener(new View.OnClickListener() {
